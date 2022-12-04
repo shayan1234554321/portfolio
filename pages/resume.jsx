8f05099ed8be6e741608fd430257f5dd67699fc7
shayan1234554321 <kshayan091@gmail.com>
2022-12-05 01:29:30 +0500

    Updated CV

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -12,7 +12,7 @@ import Sign from '../public/sign'
 import Head from "next/head";
 import Menu from '../components/menu'
 
-const cvLink = "https://docs.google.com/document/d/10tntZQ5PIt54Qikpvht0tLixE9nouJvW/edit?usp=sharing&ouid=101156306051286708216&rtpof=true&sd=true"
+const cvLink = "https://docs.google.com/document/d/1yxqgxfBkxTRi8k6IqXbVoS-z4DkQ4NP-/edit?usp=sharing&ouid=101156306051286708216&rtpof=true&sd=true"
 
 function Resume() {
 
